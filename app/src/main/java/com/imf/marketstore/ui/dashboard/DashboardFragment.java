b1fa68a5ca4f78fe3689538139ca523bf99002e7
Alex Walls <shxzmk@gmail.com>
2024-03-27 17:52:48 +0100

    Buttons with Listeners

--- app/src/main/java/com/imf/marketstore/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/imf/marketstore/ui/dashboard/DashboardFragment.java
@@ -1,6 +1,7 @@
 package com.imf.marketstore.ui.dashboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +26,23 @@ public class DashboardFragment extends Fragment {
 
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        binding.buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Llamar a la función al hacer clic en el botón
+                buyProducts();
+            }
+        });
+
+        binding.sellButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Llamar a la función al hacer clic en el botón
+                sellProducts();
+            }
+        });
+
         return root;
     }
 
@@ -32,5 +50,13 @@ public class DashboardFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void sellProducts() {
+        Log.d("MainActivity", "Vender Productos");
+    }
+
+    private void buyProducts() {
+        Log.d("MainActivity", "Comprar Productos");
     }
 }
